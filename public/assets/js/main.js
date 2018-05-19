2c679e2b76e14a4ad507f41d7525a5e065f285ab
Allan Jones <iamallanjones@gmail.com>
2018-05-19 13:46:19 -0500

    Added Dashboard page with styling

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,11 +1,9 @@
 // tabs to work
-$(document).ready(function(){
-    $('.tabs').tabs();
-  });
-  
+$(document).ready(function() {
+  $(".tabs").tabs();
+});
+
 // Initialize Mobile Nav - Sidebar
 $(document).ready(function() {
-    $(".sidenav").sidenav();
-  });
-  
-  
+  $(".sidenav").sidenav();
+});
