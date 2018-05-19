95c2a16e4b8894c42dbfbfb0334916aefd08e31e
TyPhyter <tylerjenkins08@gmail.com>
2018-05-19 11:43:12 -0500

    project controller routes implemented

--- server.js
+++ server.js
@@ -21,8 +21,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 const router = require('./routes/router');
 const userRouter = require('./controllers/user-controller');
+const projectRouter = require('./controllers/project-controller');
 app.use('/', router);
 app.use('/', userRouter);
+app.use('/', projectRouter);
 db.sequelize.sync().then(function() {
     app.listen(PORT, function () {
         console.log("App listening on PORT " + PORT);
