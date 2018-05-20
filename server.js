5b448e1774746a64da61d41a90498a47a146fbce
TyPhyter <tylerjenkins08@gmail.com>
2018-05-20 00:26:27 -0500

    enabling preflighted requests, apparently

--- server.js
+++ server.js
@@ -14,12 +14,8 @@ const PORT = process.env.PORT || 8080;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+app.use(cors());
+app.options('*', cors())
 
 const exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
